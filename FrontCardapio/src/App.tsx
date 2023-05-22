0173c2f8c32c1bea89c3d6643d6b817abcd403b6
devlooppear <115846304+devlooppear@users.noreply.github.com>
2023-05-22 16:18:01 -0300

    add style

--- FrontCardapio/src/App.tsx
+++ FrontCardapio/src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import { Card } from "./components/card/card"
-import { FoodData } from './interface/FoodData';
 import { useFoodData } from './hooks/useFoodData'
+import { CreateModal } from './components/create-modal/create-modal';
 
 
 function App() {
   const { data } = useFoodData();
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalOpen(prev => !prev)
+  }
 
   return (
     <div className='container'>
@@ -20,7 +25,9 @@ function App() {
           />
         )}
       </div>
-    </div>
+        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
+        <button onClick={handleOpenModal}>novo</button>
+      </div>
   )
 }
 
--- FrontCardapio/src/components/create-modal/create-modal.tsx
+++ FrontCardapio/src/components/create-modal/create-modal.tsx
@@ -1,37 +1,61 @@
-import { useState } from "react"
+import { useEffect, useState, Dispatch, SetStateAction } from 'react';
+import { useFoodDataMutate } from '../../hooks/useFoodDataMutate';
+import { FoodData } from '../../interface/FoodData';
+
+import "./modal.css";
 
 interface InputProps {
-    label: string,
-    value: string | number,
-    updateValue(value: any): void
+    label: string;
+    value: string | number;
+    updateValue: Dispatch<SetStateAction<string | number>>;
 }
 
-const Input = ({label, value, updateValue}: InputProps) => {
+interface ModalProps {
+    closeModal(): void;
+}
+
+const Input = ({ label, value, updateValue }: InputProps) => {
     return (
-        <div>
-            
+        <>
             <label>{label}</label>
             <input value={value} onChange={event => updateValue(event.target.value)}></input>
-            
-        </div>
-    )
+        </>
+    );
 }
 
-export function CreateModal(){
-    const [title, setTitle] = useState('')
-    const [price, setPrice] = useState(0)
-    const [image, setImage] = useState('')
+export function CreateModal({ closeModal }: ModalProps) {
+    const [title, setTitle] = useState<string>("");
+    const [price, setPrice] = useState<number>(0);
+    const [image, setImage] = useState<string>("");
+    const { mutate, isSuccess, isLoading } = useFoodDataMutate();
 
-    return(
+    const submit = () => {
+        const foodData: FoodData = {
+            title,
+            price,
+            image
+        };
+        mutate(foodData);
+    }
+
+    useEffect(() => {
+        if (!isSuccess) return;
+        closeModal();
+    }, [isSuccess, closeModal]);
+
+    return (
         <div className="modal-overlay">
             <div className="modal-body">
                 <h2>Cadastre um novo item no cardápio</h2>
                 <form className="input-container">
-                    <Input label="title" value={title} updateValue={setTitle}/>
-                    <Input label="price" value={price} updateValue={setPrice}/>
-                    <Input label="image" value={image} updateValue={setImage}/>
+                    <Input label="title" value={title} updateValue={setTitle as Dispatch<SetStateAction<string | number>>} />
+                    <Input label="price" value={price} updateValue={setPrice as Dispatch<SetStateAction<string | number>>} />
+                    <Input label="image" value={image} updateValue={setImage as Dispatch<SetStateAction<string | number>>} />
                 </form>
+                <button onClick={submit} className="btn-secondary">
+                    {isLoading ? 'postando...' : 'postar'}
+                </button>
             </div>
         </div>
-    )
+    );
 }
